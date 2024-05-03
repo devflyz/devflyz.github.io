907836d6d2e06331d2731f1fca17ed4dfd5a0704
Flyz <154094054+devflyz@users.noreply.github.com>
2024-05-03 13:02:15 +0200

    Create ApruebaONo.java

---
+++ check/ApruebaONo.java
@@ -0,0 +1,21 @@
+{
+  "ApruebaONo.java": {
+    "params_count": 2,
+    "outputs_count": 1,
+    "total_test_cases": 3,
+    "test_cases": [
+      {
+        "inputs": ["1\nAitor Tilla 5.0"],
+        "expected_outputs": ["Aitor Tilla: aprueba"]
+      },
+      {
+        "inputs": ["2\nLola Mento 4.9\nJorge Nitales 5.1"],
+        "expected_outputs": ["Lola Mento: suspende\nJorge Nitales: aprueba"]
+      },
+      {
+        "inputs": ["3\nInés Queleto 9.1\nMarta Tuada 7.5\nPaco Jones 3.1"],
+        "expected_outputs": ["Inés Queleto: aprueba\nMarta Tuada: aprueba\nPaco Jones: suspende"]
+      }
+    ]
+  }
+}
